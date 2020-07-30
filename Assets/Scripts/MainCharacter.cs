122c84f3e8a352af24314e17171c09ed14fe9220
Hugo Reis <hugohpreis@gmail.com>
2020-07-29 23:07:11 -0300

    0.0 - Alterados scripts Gravity, MainCharacter e CameraFollow.

--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -4,16 +4,23 @@ public class MainCharacter : MonoBehaviour
 {
     private CharacterController _controller;
     private MainCharacterMovement mainCharacterMovement;
+    private Gravity gravity;
 
     private void Start()
     {
         _controller = GetComponent<CharacterController>();
+
         mainCharacterMovement = gameObject.AddComponent<MainCharacterMovement>();
-        mainCharacterMovement.Init(_controller);        
+        mainCharacterMovement.Init(_controller);
+
+        gravity = gameObject.AddComponent<Gravity>();
+        gravity.Init(_controller);
     }
 
     private void FixedUpdate()
-    {       
+    {
+        gravity.CreateGravity();
+
         mainCharacterMovement.IsRunning = Input.GetKey(KeyCode.LeftShift);
         mainCharacterMovement.Move();
     }
